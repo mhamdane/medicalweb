aac59c95c74c77f7673cd333843ec4c43518d68f
Hicham SERHANI <hserhani@percallgroup.com>
2018-12-07 15:23:35 +0100

    Editing the authentication package name

--- src/main/java/io/medicalweb/server/controller/authentication/AuthenticationController.java
+++ src/main/java/io/medicalweb/server/controller/authentication/AuthenticationController.java
@@ -1,4 +1,4 @@
-package io.medicalweb.server.controller.login;
+package io.medicalweb.server.controller.authentication;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
